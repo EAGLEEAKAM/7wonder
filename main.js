122618ab3ec5512cfa89a2f8364a847def00a9b5
EAGLEEAKAM <77165496+EAGLEEAKAM@users.noreply.github.com>
2021-05-08 12:22:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function one(){
+window.alert("The Taj Mahal was constructed under the reign and guidance of the famous Mughal Emperor Shah Jahan. This monument was built as on ode to his beautiful wife Mumtaz mahal and the love between them. ... In 1983, UNESCO declared Taj Mahal as a Heritage Site. The structure of the tomb is made from the finest white marble.");
+}
+
+function two(){
+window.alert("Chichen Itza was a large Mayan city by the Maya civilization. ... There are many temples and pyramids in Chichen Itza. Tourists, or visitors, were once able to climb some of the pyramids, but it was quite challenging because the steps are very steep and small. Climbing is no longer permitted.");
+}
+
+function three(){
+    window.alert("The Art Deco-style Christ the Redeemer statue has been looming over the Brazilians from upon Corcovado mountain in an awe-inspiring state of eternal blessing since 1931. The 130-foot reinforced concrete-and-soapstone statue was designed by Heitor da Silva Costa and cost approximately $250,000 to build - much of the money was raised through donations. The statue has become an easily recognized icon for Rio and Brazil.");
+}
+
+function four(){
+    window.alert("It had seating for 50,000 people. It was 156 metres (512 ft) wide, 189 metres (620 ft) long and 57 metres (187 ft) tall. It is the biggest amphitheatre built by the Roman Empire. The Colosseum is in Rome, the capital of Italy.");
+}
+
+function five(){
+   window.alert("The Great Wall of China is an ancient wall in China. The wall is made of cement, rocks, bricks, and dirt. It was finished in 1878 and it was meant to protect the north of the empire of China from enemy attacks. It is the longest structure humans have ever built.");
+}
+
+function six(){
+    window.alert("Machu Picchu is a 15th-century Inca site located on a ridge between the Huayna Picchu and Machu Picchu mountains in Peru. It sits 7,970 feet (2,430 meters) above sea level on the eastern slope of the Andes and overlooks the Urubamba River hundreds of feet below.");
+}
+ 
+function seven(){
+    window.alert("Machu Picchu is a 15th-century Inca site located on a ridge between the Huayna Picchu and Machu Picchu mountains in Peru. It sits 7,970 feet (2,430 meters) above sea level on the eastern slope of the Andes and overlooks the Urubamba River hundreds of feet below.");
+}
+
+
